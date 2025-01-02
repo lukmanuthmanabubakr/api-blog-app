a01c7eb684e34ec02f1ca0ef51d085e088c48928
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-01-02 14:06:02 +0100

    updated

--- index.js
+++ index.js
@@ -22,3 +22,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+// FILE STORAGE
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/assets")
+    }
+})
