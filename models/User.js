28e1c36b4e2b5bf183d9df2a1b776baaca812617
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-01-03 23:07:12 +0100

    progressing

---
+++ models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 50,
+    unique: true,
+    trim: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please enter a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 5,
+  },
+  picturePath: {
+    type: String,
+    required: true,
+    min: 5,
+  },
+});
