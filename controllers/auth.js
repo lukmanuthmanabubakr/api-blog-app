106481bd81cb6f42690b7b3f4e43344d5e4e37e2
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-01-04 21:04:26 +0100

    progress

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,6 +16,20 @@ export const register = async (req, res) => {
       occupation,
     } = req.body;
 
-    const salt = await bcrypt.genSalt()
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000)
+    });
   } catch (err) {}
 };
