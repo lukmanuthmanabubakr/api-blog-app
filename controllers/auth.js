e9a96d66bacfb9009ac3c2c02ab100b64691a616
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-01-04 20:37:54 +0100

    lets distructure

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,3 +3,17 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 // REGISTER USER
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+  } catch (err) {}
+};
