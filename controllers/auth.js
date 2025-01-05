dc7e09abdc54bd73c1b3997c7b8eceb263eadd3b
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-01-05 15:14:57 +0100

    Error message

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,9 +29,11 @@ export const register = async (req, res) => {
       location,
       occupation,
       viewedProfile: Math.floor(Math.random() * 10000),
-      impressions: Math.floor(Math.random() * 10000)
+      impressions: Math.floor(Math.random() * 10000),
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
-  } catch (err) {}
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
